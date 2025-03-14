3f2206524c032db53864fea6ccbbd6999d29fbc8
Muhammad Noman Siddiqui <145224593+siddiquieses@users.noreply.github.com>
2025-03-14 14:17:59 +0500

    Add files via upload

---
+++ CSS/index.js
@@ -0,0 +1,5 @@
+// Toggle menu visibility on click
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+});
